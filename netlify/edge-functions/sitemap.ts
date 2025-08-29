3f5b1981466e17fd8430f317d98720404894afaa
Floare08 <iordanflorin228@gmail.com>
2025-08-29 03:56:44 +0200

    Create sitemap.ts

---
+++ netlify/edge-functions/sitemap.ts
@@ -0,0 +1,23 @@
+export default async (request: Request) => {
+  const url = new URL(request.url);
+  const origin = `${url.protocol}//${url.host}`;
+  const paths = ["/", "/ro", "/en"];
+  const toUrl = (p: string) => `${origin}${p}`;
+  const body =
+    `<?xml version="1.0" encoding="UTF-8"?>` +
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` +
+    paths.map(p => `
+  <url>
+    <loc>${toUrl(p)}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>${p === "/" ? "1.0" : "0.8"}</priority>
+  </url>`).join("") +
+    `
+</urlset>`;
+  return new Response(body, {
+    headers: {
+      "content-type": "application/xml; charset=utf-8",
+      "cache-control": "public, max-age=86400"
+    }
+  });
+};
