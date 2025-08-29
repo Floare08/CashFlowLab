316958fdaea35e3af08de1243fff22c86e583dcb
Floare08 <iordanflorin228@gmail.com>
2025-08-29 03:57:33 +0200

    Update sitemap.ts

--- netlify/edge-functions/sitemap.ts
+++ netlify/edge-functions/sitemap.ts
@@ -1,19 +1,22 @@
 export default async (request: Request) => {
   const url = new URL(request.url);
   const origin = `${url.protocol}//${url.host}`;
-  const paths = ["/", "/ro", "/en"];
-  const toUrl = (p: string) => `${origin}${p}`;
+
+  // Listează rutele reale ale site-ului tău
+  const paths = ["/", "/en/"];
+
   const body =
     `<?xml version="1.0" encoding="UTF-8"?>` +
     `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">` +
     paths.map(p => `
   <url>
-    <loc>${toUrl(p)}</loc>
+    <loc>${origin}${p}</loc>
     <changefreq>weekly</changefreq>
     <priority>${p === "/" ? "1.0" : "0.8"}</priority>
   </url>`).join("") +
     `
 </urlset>`;
+
   return new Response(body, {
     headers: {
       "content-type": "application/xml; charset=utf-8",
